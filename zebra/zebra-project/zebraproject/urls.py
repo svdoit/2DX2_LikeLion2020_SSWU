9993c51911b5b6d3e01cdf74b0cc105023c288c0
김지수 <kingwltn@gmail.com>
2020-07-23 17:18:22 +0900

    main.html을 띄우려고 노력했습니다.
    main.html을 띄우려고 노력했으나 실패했습니다. 더 분발하겠습니다.

--- zebra/zebra-project/zebraproject/urls.py
+++ zebra/zebra-project/zebraproject/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from zebraapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name= "main"),
 ]
