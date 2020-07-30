83f0c275ec9cdeaccf15014ae01d8dd6861e08ff
LAPTOP-9HAASIOL\jiuuucy <jiuuucy@gmail.com>
2020-07-30 13:19:48 +0900

    views.py에 tip, tipDetail 등록
    views.py에 팁 관련 함수 추가함

--- zebra/zebra-project/zebraapp/views.py
+++ zebra/zebra-project/zebraapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import MyItem
+from .models import Tip
 
 # Create your views here.
 
@@ -10,5 +11,14 @@ def main(request):
 def myLevel(request):
     myItems_myLevel = MyItem.objects.all()
     return render(request, 'myLevel.html', {'myItems':myItems_myLevel})
+
+def tip(request):
+    tips = Tip.objects.all()
+    return render(request, 'tip.html', {'tips':tips})
+
+def tipDetail(request, tip_id):
+    tip_detail = get_object_or_404(Tip, pk=tip_id)
+    return render(request, 'tipDetail.html', {'tip':tip_detail})
+
 
 
