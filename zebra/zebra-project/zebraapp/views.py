6e2e70113fd2434315c3c758f94670a1b1b81ec3
svdoit <doemily@naver.com>
2020-07-30 16:31:55 +0900

    myLevel 페이지 추가 -myItem 등록, 삭제, 수정 구현 -myItem 디테일 페이지 구현

--- zebra/zebra-project/zebraapp/views.py
+++ zebra/zebra-project/zebraapp/views.py
@@ -16,6 +16,43 @@ def myLevel(request):
     myItems_myLevel = MyItem.objects.all()
     return render(request, 'myLevel.html', {'myItems':myItems_myLevel})
 
+def submit_myItem(request):
+    return render(request, 'submit_myItem.html')
+
+def create(request):
+    myItems = MyItem()
+    myItems.myItemName = request.GET['myItemName']
+    myItems.myItemStore = request.GET['myItemStore']
+    myItems.myItemDate = request.GET['myItemDate']
+    myItems.myItemCat = request.GET['myItemCat']
+    myItems.save()
+    return redirect('myLevel')
+
+def detail_myItem(request, my_Items_id):
+    myItems_detail = get_object_or_404(MyItem, pk=my_Items_id)
+    return render(request, 'detail_myItem.html', {'my_Items':myItems_detail})
+
+def update_myItem(request, my_Items_id):
+    my_Items = get_object_or_404(MyItem, pk=my_Items_id)
+
+    if request.method == "POST":
+        myItemName = request.POST.get('myItemName')
+        myItemStore = request.POST.get('myItemStore')
+        myItemDate = request.POST.get('myItemDate')
+        myItemCat = request.POST.get('myItemCat')
+        my_Items.myItemName = myItemName
+        my_Items.myItemStore = myItemStore
+        my_Items.myItemDate = myItemDate
+        my_Items.myItemCat = myItemCat
+        my_Items.save()
+        return redirect('detail', my_Items.id)
+    return render(request, 'update_myItem.html', {'my_Items':my_Items})
+
+def delete_myItem(request, my_Items_id):
+    myItems = get_object_or_404(MyItem, pk=my_Items_id)
+    myItems.delete()
+    return redirect('myLevel')
+
 def tip(request):
     tips = Tip.objects.all()
     return render(request, 'tip.html', {'tips':tips})
@@ -30,4 +67,5 @@ def product(request):
 def productDetail(request):
     productDetails = Item.objects.all()
     return render(request, 'productDetail.html',{'productDetails':productDetails})
+
 
