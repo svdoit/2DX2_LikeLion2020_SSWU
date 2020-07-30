c1324c74385f3594aaa341298da6d76241363d87
LAPTOP-9HAASIOL\jiuuucy <jiuuucy@gmail.com>
2020-07-30 13:16:48 +0900

    tip html 수정 및 tip model 등록
    tip.html,tipDetail.html 수정하고 models.py에 tip 모델 등록하고 migration함

--- zebra/zebra-project/zebraapp/models.py
+++ zebra/zebra-project/zebraapp/models.py
@@ -12,3 +12,15 @@ class MyItem(models.Model):
 
     def __str__(self):
         return self.myItemDate.strftime("%Y.%m.%d") + "-" + self.myItemName + "-" + self.myItemStore + "-" + self.myItemCat
+
+class Tip(models.Model):
+    objects = models.Manager()
+    tipTitle = models.CharField(max_length=200)
+    # 추후 작성자 추가 기능 필요한지 논의
+    # tipAuthor = models.ForeignKey(settings.AUTH_USER_MODEL)
+    tipDate = models.DateField('date published')
+    tipBody = models.TextField()
+    tipImg = models.ImageField(blank=True, upload_to="images", null=True)
+
+    def __str__(self):
+        return self.tipTitle
